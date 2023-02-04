1aec0b24f61a3edd8490d512821a1cd6ef69d2b9
hunkicho <jgchk4814@gmail.com>
2023-02-04 15:54:55 +0900

    [FEAT] 도커 설정
    배포를 위한 도커 설정.

---
+++ Dockerfile
@@ -0,0 +1,20 @@
+#도커 이미지를 노드 16.19 기반으로 만든다.
+FROM node:16.19.0-alpine 
+
+# app라는 디렉토리 만들고 워크 디렉토리로 지정
+# 파일들을 app 아래로 복사
+RUN mkdir -p /app
+WORKDIR /app
+ADD . /app/
+
+# RUN npm install -g pnpm
+# RUN pnpm install
+# RUN pnpm build
+
+RUN rm yarn.lock || true && rm package-lock.json || true
+RUN npm install -g yarn && yarn install && yarn build
+
+ENV HOST 0.0.0.0
+EXPOSE 3000
+
+CMD ["node", ".output/server/index.mjs"]
---
+++ config/scripts/deploy.sh
@@ -0,0 +1,29 @@
+# !/bin/bash
+
+# docker가 없다면, docker 설치
+if ! type docker > /dev/null
+then
+  echo "docker does not exist"
+  echo "Start installing docker"
+  sudo apt-get update
+  sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
+  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+  sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+  sudo apt update
+  apt-cache policy docker-ce
+  sudo apt install -y docker-ce
+fi
+
+# docker-compose가 없다면 docker-compose 설치
+if ! type docker-compose > /dev/null
+then
+  echo "docker-compose does not exist"
+  echo "Start installing docker-compose"
+  sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+  sudo chmod +x /usr/local/bin/docker-compose
+fi
+
+echo "start docker-compose up: ubuntu"
+docker ps -a | grep hunkicho/jg_front_vue:latest| awk '{print$1}' | xargs -t -I % docker rm -f % && docker image ls | grep hunkicho/jg_front_vue:latest | awk '{print$3}' | xargs -I % docker rmi %
+docker ps -a | grep hunkicho/jg_front_nginx:latest | awk '{print$1}' | xargs -t -I % docker rm -f % && docker image ls | grep hunkicho/jg_front_nginx:latest | awk '{print$3}' | xargs -I % docker rmi %
+cd ~ && sudo docker-compose pull &&  sudo docker-compose -f docker-compose.yml up --build -d
