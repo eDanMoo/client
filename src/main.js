50cc121a8446c4fe749199fbd1305700d65194f1
hunkicho <jgchk4814@gmail.com>
2023-03-05 15:31:17 +0900

    [FEAT] sentry 설정 변경
    sentry 설정 변경.

--- src/main.js
+++ src/main.js
@@ -20,18 +20,23 @@ Sentry.init({
             tracePropagationTargets: ["localhost", "koword.com", /^\//],
         }),
     ],
+    tracingOptions: {
+        // 하위 구성 요소를 추적하고 렌더링 프로세스에 대한 자세한 내용을 보기
+        trackComponents: true,
+    },
+    attachProps: true,
     // Set tracesSampleRate to 1.0 to capture 100%
     // of transactions for performance monitoring.
     // We recommend adjusting this value in production
     tracesSampleRate: 1.0,
 });
 
+app.config.errorHandler = async (error, vm, info) => {
+    Sentry.captureException(error);
+};
+
 app.use(createPinia());
 app.use(router);
 // app.use(mixins);
-
-app.config.errorHandler = (error, vm, info) => {
-    Sentry.captureException(error);
-};
 
 app.mount("#app");
